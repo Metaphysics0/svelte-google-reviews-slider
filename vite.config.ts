9bf344061ad71b036b40c7443487006c9ff29c5a
Ryan Roberts <ryanr@cityhive.net>
2023-09-12 09:15:13 +0300

    final build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [svelte(), cssInjectedByJsPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "plugin.js",
+        assetFileNames: "plugin.css",
+        chunkFileNames: "chunk.js",
+        manualChunks: undefined,
+      },
+    },
+  },
+});
